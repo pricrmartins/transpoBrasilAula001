2cef2abb04ee4d18052e604b030601f4c32773e5
suporte <suporte@LAB01_PC15>
2018-09-15 14:05:08 -0300

    Atividades de operadores matemáticos

---
+++ src/br/com/cursojava/aula001/CalculoMedia.java
@@ -0,0 +1,12 @@
+package br.com.cursojava.aula001;
+
+public class CalculoMedia {
+	public static void main(String[] args){
+		//05
+		double nota1 = 10;
+		double nota2 = 9;
+		double nota3 = 8;
+		
+		System.out.println("A média é: "+ (nota1+nota2+nota3)/3);
+	}
+}
